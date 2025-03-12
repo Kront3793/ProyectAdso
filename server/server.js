e25d57016e8bf4ca72009674eb3071ffc455bc4f
kevin <kjcaceres990@soy.sena.edu.co>
2025-03-12 12:37:58 -0500

    se sube backend de login

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+
+import connectDB from './config/mongodb.js'
+import authRouther from './routes/authRoutes.js'
+import userRouter from "./routes/userRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+
+const app = express();
+const port = process.env.PORT || 4000;  
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}))
+
+//API endpoints
+app.get('/', (req, res) => res.send("API WORKING"));
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+
+
+
+
+app.listen(port, ()=> console.log(`SERVER RUNNING IN PORT:${port}`));   
